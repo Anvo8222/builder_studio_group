4bdbdb07a998f1f272f92c22421071a2dad7b5f0
Nguyen An <anvo8222@gmail.com>
2022-05-05 14:04:12 +0700

    change logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import logo from "../../images/logo.png";
 
 function Logo(props) {
   return (
     <div className="h-[64px] sm:min-w-[200px] min-w-[262px] sm:border-r-0 border-r-2 border-solid border-inherit">
-      Aff Coder
+      <img className="w-[230px] ml-[30px] h-[100%]" src={logo} alt="logo" />
     </div>
   );
 }
