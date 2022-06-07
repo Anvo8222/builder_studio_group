67fadab6dba7dfef99fcb50011fffa9c101961e3
Nguyen An <anvo8222@gmail.com>
2022-06-07 14:57:08 +0700

    update

--- src/Slice/changeCurrency.js
+++ src/Slice/changeCurrency.js
@@ -1,16 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { BsCurrencyYen } from "react-icons/bs";
 
 const currencySlice = createSlice({
   name: "changeCurrency",
   initialState: 8,
-  // {
-  //   id: 8,
-  //   name: "USD",
-  //   shortName: "USD",
-  //   icon: BsCurrencyYen,
-  //   value: 128.11,
-  // }
+
   reducers: {
     currencyValue(state, action) {
       // eslint-disable-next-line no-unused-expressions
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,18 +1,16 @@
 import { Navigate } from "react-router-dom";
-import HomePage from "../containers/HomePage";
-import AdminPage from "../containers/AdminPage";
-import LoginPage from "../containers/LoginPage";
-import CategoryPage from "../containers/CategoryPage";
-import ProductPage from "../containers/ProductPage";
 import PageNotFound from "../components/PageNotFound";
 import {
   CATEGORY_PAGE,
-  HOME,
   HOME_PAGE_ADMIN,
   LOGIN_PAGE_ADMIN,
   PRODUCT_PAGE,
 } from "../config";
-import Appp from "../Appp";
+import AdminPage from "../containers/AdminPage";
+import CategoryPage from "../containers/CategoryPage";
+import HomePage from "../containers/HomePage";
+import LoginPage from "../containers/LoginPage";
+import ProductPage from "../containers/ProductPage";
 
 const routes = (isLoggedIn) => [
   {
@@ -23,10 +21,7 @@ const routes = (isLoggedIn) => [
     path: "/",
     element: <HomePage />,
   },
-  {
-    path: "/test",
-    element: <Appp />,
-  },
+
   {
     path: HOME_PAGE_ADMIN,
     element: isLoggedIn ? <AdminPage /> : <Navigate to="/" />,
